7b8768208ff8dca87aceef9a02c522c2b4861084
Daniil Rassadin <daniil@rassadin.org>
2023-12-20 17:00:30 +0600

    Storyboard is deleted

---
+++ Flights/TabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarController.swift
+//  Flights
+//
+//  Created by Daniil Rassadin on 20/12/23.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addTabBarItems()
+    }
+    
+    // MARK: - Setup UI
+    
+    private func addTabBarItems() {
+        
+    }
+}
