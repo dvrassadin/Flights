d3036d8a58c54f7934d51fd4cd1a8d9ffc134f14
Daniil Rassadin <daniil@rassadin.org>
2023-12-20 16:32:50 +0600

    Initial Commit

---
+++ Flights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flights
+//
+//  Created by Daniil Rassadin on 20/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
